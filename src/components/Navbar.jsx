577bb9a394ea1665e58ba1ec081b3374c49c03ed
ddnamn <notimportantfile931900@gmail.com>
2023-06-21 23:32:35 +0700

    fix repo file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import { HashLink as Link } from "react-router-hash-link"
+import pImage from '../assets/profileImage.png'
 
 export default function Navbar() {
   return (
     <>
-     <nav className="h-screen w-72 fixed bg-teal-200 flex flex-col justify-center items-center space-y-12 shadow-2xl">
-        <div className="w-40 h-40 rounded-full bg-black border-white border-4 flex-shrink-0"></div>
+     <nav className="h-screen w-72 fixed bg-teal-200 flex flex-col justify-center items-center space-y-12 shadow-2xl ">
+        <div className="w-40 h-40 rounded-full bg-black border-white border-4 flex-shrink-0 overflow-hidden  flex justify-center items-center hover:-translate-y-[2px] " >
+          <img src={pImage} alt="profile" className=" h-full"/>
+        </div>
         <ul className=" w-full space-y-4 font-light text-3xl flex flex-col items-center" >
-          <li><Link smooth to='#home'>home</Link></li>
-          <li><Link smooth to='#about'>about</Link></li>
-          <li><Link smooth to='#experience'>expeience</Link></li>
-          <li><Link smooth to='#skills'>skills</Link></li>
-          <li><Link smooth to='#interests'>interests</Link></li>
-          <li><Link smooth to='#awards'>awards</Link></li>
+          <li className="hover:border-b-black hover:border-b-2 hover:box-content"><Link smooth to='#home'>home</Link></li>
+          <li className="hover:border-b-black hover:border-b-2 hover:box-content"><Link smooth to='#about' >about</Link></li>
+          <li className="hover:border-b-black hover:border-b-2 hover:box-content"><Link smooth to='#experience'>expeience</Link></li>
+          <li className="hover:border-b-black hover:border-b-2 hover:box-content"><Link smooth to='#skills'>skills</Link></li>
+          <li className="hover:border-b-black hover:border-b-2 hover:box-content"><Link smooth to='#interests'>interests</Link></li>
+          <li className="hover:border-b-black hover:border-b-2 hover:box-content"><Link smooth to='#awards'>awards</Link></li>
         </ul>
       </nav> 
     </>
