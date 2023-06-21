439acec8995830a6cbc3e0fe011e387e0f9a380a
ddnamn <notimportantfile931900@gmail.com>
2023-06-21 23:47:14 +0700

    fix error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,16 @@
 export default function Home() {
   return (
     <>
-      <section id="home" className="h-screen ml-80">
+      <section id="home" className="h-screen ml-80 ">
         <h1 className=".title">Home</h1>
-        <p className="w-3/4 font-bold indent-10 pt-10 text-3xl">Assalamu'alaikum Wr.Wb, I am Muhammad Aminuddin Rusydi, an Informatics Engineering student at State University of Surabaya. I am an active student bot in academic andd organization. i able to work individal and team, self-learner and commit to develop my skills continuously. let know more about me at here </p>
+        <p className="w-3/4  indent-10 pt-10 text-3xl ">Assalamu'alaikum Wr.Wb, I am Muhammad Aminuddin Rusydi, an Informatics Engineering student at State University of Surabaya. I am an active student bot in academic andd organization. i able to work individal and team, self-learner and commit to develop my skills continuously. let know more about me at here </p>
+        <p className="w-3/4  indent-10 pt-10 text-3xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis atque sint quasi, facere modi adipisci ipsam, repudiandae animi quod nihil placeat voluptas velit excepturi fuga, totam maiores possimus nam. Pariatur accusantium sequi dicta ex alias quas cumque quibusdam autem? Ut. </p>
+        <ul className="list-disc ml-11 indent-10 pt-10 text-3xl">
+          <li>Javascript</li>
+          <li>React</li>
+          <li>React Native</li>
+          <li>Tailwind</li>
+        </ul>
       </section> 
     </>
   )
--- src/pages/Interest.jsx
+++ src/pages/Interest.jsx
@@ -2,7 +2,7 @@
 export default function Interest() {
   return (
     <>
- <section id="interests" className="h-screen border-t-2 border-black ml-80">
+      <section id="interests" className="h-screen border-t-2 border-black ml-80">
         <h1>interests</h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, deleniti.</p>
       </section>
